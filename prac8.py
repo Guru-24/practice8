ee2add77d3c33a229ec8bb13d6fd25bf11885f9d
Guru-24 <guru.g@s.amity.edu>
2025-07-25 23:49:17 +0530

    compeleted the program

--- prac8.py
+++ prac8.py
@@ -1,19 +1,13 @@
-#include <stdio.h>
-int main()
-{
-  int n, reversed = 0, remainder, original;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    original = n;
-while (n != 0) {
-        remainder = n % 10;
-        reversed = reversed * 10 + remainder;
-        n /= 10;
-    }
-if (original == reversed)
-        printf("%d is a palindrome.", original);
-    else
-        printf("%d is not a palindrome.", original);
+def is_palindrome(text):
 
-    return 0;
-}
+    text = text.replace(" ", "").lower()
+    return text == text[::-1]
+
+user_input = input("Enter a word or phrase to check for palindrome: ")
+
+
+if is_palindrome(user_input):
+    print("✅ It is a palindrome.")
+else:
+    print("❌ It is not a palindrome.")
+
