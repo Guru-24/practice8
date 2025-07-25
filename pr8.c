ef9f8b50e72f687bb7095c5ade4e3e091b4ed506
Guru-24 <guru.g@s.amity.edu>
2025-07-25 23:57:35 +0530

    initialization done

--- pr8.c
+++ pr8.c
@@ -4,3 +4,15 @@ include <stdio.h>
 long long convert(long long);
 
 int main()
+	{
+
+    long long n;
+
+    printf("Enter a binary number: ");
+    scanf("%lld", &n);
+
+    printf("%lld in binary = %lld in decimal", n, convert(n));
+
+    return 0;
+}
+
