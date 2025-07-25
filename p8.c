50c4561592be1d225a136ca494ac2375925fe8e5
Guru-24 <guru.g@s.amity.edu>
2025-07-25 23:54:26 +0530

    done

--- p8.c
+++ p8.c
@@ -12,3 +12,10 @@ int main(){
      
        originalNum /= 10;
     }
+     if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
